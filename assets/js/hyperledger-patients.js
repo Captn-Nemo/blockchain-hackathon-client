49f84c15f5e8237a63cbb947f04e9367f77816ab
Kenneth Li <kenkraken@gmail.com>
2017-09-24 10:58:26 -0500

    started updateMedication form

--- assets/js/hyperledger-patients.js
+++ assets/js/hyperledger-patients.js
@@ -1,4 +1,4 @@
-function selectPatient(event){
+function selectPatient(event) {
     console.log(event);
 }
 
@@ -8,9 +8,9 @@ function getPatients() {
     xhttp.send();
     var res = xhttp.responseText;
     res = JSON.parse(res);
-    for(var person in res){
-        $("#patient-dropdown").append("<li onselect=selectPatient(event)>" + res[person].firstname + " "
-            + res[person].lastname + "</li>");
+    for (var person in res) {
+        $("#patient-dropdown").append("<li onselect=selectPatient(event)>" + res[person].firstname + " " +
+            res[person].lastname + "</li>");
     }
 
     xhttp.open("GET", "http://10.66.116.98:3000/api/composers.healthrecords.updateMedication", false);
@@ -21,13 +21,13 @@ function getPatients() {
     // pretend there's only one patient
     var medArr = res[0].medicationArray;
     console.log(medArr);
-    for(var i=0; i<medArr.length; i++){
-      $('#med-desc').append("<tr><td>" + medArr[i] + "</td></tr>");
+    for (var i = 0; i < medArr.length; i++) {
+        $('#med-desc').append("<tr><td>" + medArr[i] + "</td></tr>");
     }
 
 }
 
-$('#patient-dropdown').on('click', '.btn-select', function (e) {
+$('#patient-dropdown').on('click', '.btn-select', function(e) {
     e.preventDefault();
     var ul = $(this).find("ul");
     if ($(this).hasClass("active")) {
@@ -40,14 +40,43 @@ $('#patient-dropdown').on('click', '.btn-select', function (e) {
         }
         ul.hide();
         $(this).removeClass("active");
-    }
-    else {
-        $('.btn-select').not(this).each(function () {
+    } else {
+        $('.btn-select').not(this).each(function() {
             $(this).removeClass("active").find("ul").hide();
         });
         ul.slideDown(300);
         $(this).addClass("active");
     }
+});
+
+$('#updateMeds').submit(function(e) {
+            var xhttp = new XMLHttpRequest();
+            xhttp.open("POST", "http://10.66.116.98:3000/api/composers.healthrecords.updateMedication", false);
+            xhttp.setRequestHeader('Content-type', "application/json");
+            xhttp.send(JSON.stringify({
+                "$class": "composers.healthrecords.updateMedication",
+                "medicationArray": ["test medz"],
+                "patientInfo": {
+                    "$class": "composers.healthrecords.PatientInfo",
+                    "patientID": "patientID:0427",
+                    "name": {
+                        "$class": "composers.healthrecords.Name",
+                        "firstName": "",
+                        "lastName": ""
+                    },
+                    "medicationArray": [],
+                    "pastVisitsArray": []
+                }
+            }));
+            var res = xhttp.responseText;
+            res = JSON.parse(res);
+            console.log(res)
+
+            // var medArr = res[0].medicationArray;
+            // console.log(medArr);
+            // for (var i = 0; i < medArr.length; i++) {
+            //     $('#med-desc').append("<tr><td>" + medArr[i] + "</td></tr>");
+            // }
 });
 
 window.onload = getPatients();
